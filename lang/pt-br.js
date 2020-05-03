c697b6679a23b74eadf6b3b9ca859c2f54946462
Samuel Cleiton Silva <samuelcleiton@hotmail.com>
2020-05-02 23:08:50 -0300

    Create brazilian portuguese lang

---
+++ lang/pt-br.js
@@ -0,0 +1,11 @@
+"use strict"
+
+CKEDITOR.plugins.setLang( 'btgrid', 'pt-br', {
+	selNumCols: 'Selecione o número de colunas',
+    genNrRows: 'Adicione o número de linhas a serem geradas',
+	infoTab: 'Informações',
+	createBtGrid: 'Criar grade do Bootstrap',
+	editBtGrid: 'Editar grade do Bootstrap',
+	numColsError:  'Por favor, selecione o número de colunas.',
+	numRowsError: 'Por favor insira um valor numérico para o número de linhas.',
+} );
